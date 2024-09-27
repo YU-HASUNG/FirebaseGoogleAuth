c20d58f7f65d0a38373cddcaab7c497569f281f3
YU-HASUNG <harry.yu@kyobobook.com>
2024-09-27 11:36:52 +0900

    [240927] 프로젝트 기본설정

---
+++ app/src/main/java/com/hacoding/firebasegoogleauth/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.hacoding.firebasegoogleauth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hacoding.firebasegoogleauth.ui.theme.FirebaseGoogleAuthTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FirebaseGoogleAuthTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirebaseGoogleAuthTheme {
+        Greeting("Android")
+    }
+}
